11d2c524c30ccdb498ab9f8984ac5c9315018bf2
Rusty <rusty3925@gmail.com>
2025-01-25 11:55:46 -0800

    much cleaner

--- scripts/screens/ProfileScreen.py
+++ scripts/screens/ProfileScreen.py
@@ -538,48 +538,27 @@ class ProfileScreen(Screens):
         or for changes in the profile."""
         self.the_cat = Cat.all_cats.get(game.switches["cat"])
 
-        # use these attributes to create differing profiles for StarClan cats etc.
-        is_sc_instructor = False
-        is_df_instructor = False
-        is_ur_instructor = False
-        if self.the_cat is None:
-            return
-        if (
-                self.the_cat.dead
-                and game.clan.instructor.ID == self.the_cat.ID
-                and self.the_cat.outside
-        ):
-            is_ur_instructor = True
-        elif (
-            self.the_cat.dead
-            and game.clan.instructor.ID == self.the_cat.ID
-            and self.the_cat.df is False
-        ):
-            is_sc_instructor = True
-        elif (
-            self.the_cat.dead
-            and game.clan.instructor.ID == self.the_cat.ID
-            and self.the_cat.df is True
-        ):
-            is_df_instructor = True
-
         # Info in string
         cat_name = str(self.the_cat.name)
         cat_name = shorten_text_to_fit(cat_name, 500, 20)
         if self.the_cat.dead:
             cat_name = i18n.t("general.dead_label", name=cat_name)
-        if is_sc_instructor:
-            self.the_cat.thought = i18n.t(
-                "screens.profile.guide_thought_sc", clan=game.clan.name
-            )
-        if is_df_instructor:
-            self.the_cat.thought = i18n.t(
-                "screens.profile.guide_thought_df", clan=game.clan.name
-            )
-        if is_ur_instructor:
-            self.the_cat.thought = i18n.t(
-                "screens.profile.guide_thought_ur", clan=game.clan.name
-            )
+
+            # set guide cat thought
+            if game.clan.instructor.ID == self.the_cat.ID:
+                if self.the_cat.outside:
+                    self.the_cat.thought = i18n.t(
+                        "screens.profile.guide_thought_ur", clan=game.clan.name
+                    )
+                elif self.the_cat.df:
+                    self.the_cat.thought = i18n.t(
+                        "screens.profile.guide_thought_df", clan=game.clan.name
+                    )
+                else:
+                    self.the_cat.thought = i18n.t(
+                        "screens.profile.guide_thought_sc", clan=game.clan.name
+                    )
+
 
         self.profile_elements["cat_name"] = pygame_gui.elements.UITextBox(
             cat_name,
